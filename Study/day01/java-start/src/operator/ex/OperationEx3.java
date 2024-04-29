3ab053c58292d5a5af3ee7717391853ff7e75e92
DooYoungHo <pimang223@naver.com>
2024-04-29 17:27:11 +0900

    JAVA 스터디 1일차

---
+++ Study/day01/java-start/src/operator/ex/OperationEx3.java
@@ -0,0 +1,19 @@
+package operator.ex;
+import java.util.Scanner;
+
+public class OperationEx3 {
+
+    public static void main(String[] args) {
+        
+        int score = 81;
+        
+        boolean result = (score >= 80) && (score <= 100);
+
+        System.out.println("result = " + result);
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+
+// 추상 클래스 @abstract
